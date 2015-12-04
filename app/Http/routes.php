4f3841de159746b7ed625e52ed5bd5876a9a5ad1
Stephen Foo <stephen.frx@gmail.com>
2015-12-05 00:54:12 +0800

    modify auth user

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,5 +14,7 @@
 Route::get('/', 'CommentController@getGuestbook');
 
 Route::controllers([
-	'comment' => 'CommentController',
+	'auth'     => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+	'comment'  => 'CommentController',
 ]);
